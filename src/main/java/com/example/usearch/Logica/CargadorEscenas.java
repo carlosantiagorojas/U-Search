2d93dff6fba16c5f65d9b89c95b621ec7abca132
Javi <javisdgv@gmail.com>
2023-05-02 23:35:32 -0500

    me voy a tirar de un puente siu

--- src/main/java/com/example/usearch/Logica/CargadorEscenas.java
+++ src/main/java/com/example/usearch/Logica/CargadorEscenas.java
@@ -7,6 +7,8 @@ import com.example.usearch.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.io.IOException;
 
 public class CargadorEscenas {
@@ -23,12 +25,12 @@ public class CargadorEscenas {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml));
             Scene scene = new Scene(fxmlLoader.load(), 385, 660);
             stage.setScene(scene);
-            stage.setTitle(title);
             this.controladorGeneral = fxmlLoader.getController();
             this.controladorGeneral.setStage(stage);
             if(controladorGeneral instanceof InterfazUsuarioController){
                 ((InterfazUsuarioController) controladorGeneral).mostrarObjetosPerdidos();
             }
+
             stage.show();
 
         } catch (IOException e) {
